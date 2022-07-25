181a0c0de4c7bb7be2a60e760cbe78fb0a0b5b7a
dorcas-1995 <wayumulu58@gmail.com>
2022-07-25 00:18:29 -0400

    useState commit

--- src/components/Poem.js
+++ src/components/Poem.js
@@ -1,6 +1,13 @@
 import React from "react";
 
-function Poem() {
+function Poem({ title, author, content, onClickDeleteButton, poem }) {
+
+  const [isRead, setIsRead] = useState(false);
+
+  function handleMarkAsReadButton() {
+    setIsRead((isRead) => !isRead);
+  }
+  
   return (
     <div>
       <h3>Title</h3>
