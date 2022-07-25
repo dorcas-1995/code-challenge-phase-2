f8ea909c0b6e564b18433c75b47ea31e53c3520d
dorcas-1995 <wayumulu58@gmail.com>
2022-07-25 00:23:02 -0400

    return commit

--- src/components/Poem.js
+++ src/components/Poem.js
@@ -15,15 +15,23 @@ function Poem({ title, author, content, onClickDeleteButton, poem }) {
       .then((r) => r.json())
       .then(() => onClickDeleteButton(poem));
   }
-  
+
   return (
     <div>
-      <h3>Title</h3>
-      <p>Content</p>
+      <h3>{title}</h3>
+
+      <p style={isRead ? { color: "gray" } : { color: "black" }}>{content}</p>
+
       <p>
-        <strong>- By Author</strong>
+        <strong>- By {author}</strong>
       </p>
-      <button>Mark as read</button>
+      <button onClick={handleMarkAsReadButton}>
+        {isRead ? "Mark as unread" : "Mark as read"}
+      </button>
+      <button className="remove" onClick={handleDeleteButton}>
+        DELETE
+      </button>
+      <button className="favorites">Add To Favorites</button>
     </div>
   );
 }
